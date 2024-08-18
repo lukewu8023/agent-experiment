0063006dc065d2b9cc779e8b4b8dfaa611b665c3
Jialei Yang <y.jialei0805@gmail.com>
2024-08-18 23:22:20 +0800

    update plan output format to list of json for steps

--- prompt/plan.py
+++ prompt/plan.py
@@ -1,4 +1,4 @@
 PLAN_FORMAT = """
-Based on background and knowledge given above to generate a plan, put each step in the json format as the output, step has attribute step_name and step_description, all steps are under 'steps' root attribute.
+Based on background and knowledge given above to generate a plan, put each step in the json format as the output, step has attribute step_name and step_description, all steps should form a list under 'steps' root attribute.
 ex: step_name: Prepare eggs, step_description: Get the eggs from the fridge and put on the table.
 """
